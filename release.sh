19de444a2127e1dae88b283a76c6c7e634b71024
emrah <emrah.eryilmaz@nordeck.net>
2024-05-13 15:49:00 +0300

    chore: nginx-jwt-auth.lua into release.sh

--- release.sh
+++ release.sh
@@ -23,7 +23,12 @@ mkdir -p $BASEDIR/files/home/scaler/.cache
 export DENO_DIR=$BASEDIR/files/home/scaler/.cache/deno
 $LOCAL_DENO cache $BASEDIR/files/home/scaler/app/scaler.ts
 
-# test
+# test deno application
 $LOCAL_DENO fmt --check $BASEDIR/files/home/scaler/app/
 $LOCAL_DENO lint $BASEDIR/files/home/scaler/app/
 $LOCAL_DENO check $BASEDIR/files/home/scaler/app/scaler.ts
+
+# nginx-jwt-auth
+mkdir $BASEDIR/files/usr/local/share
+wget -O $BASEDIR/files/usr/local/share/nginx-jwt-auth.lua \
+    https://raw.githubusercontent.com/nordeck/nginx-jwt-auth/main/nginx-jwt-auth.lua
